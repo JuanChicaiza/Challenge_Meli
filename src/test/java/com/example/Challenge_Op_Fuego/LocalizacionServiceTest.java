65786f59adbd731510e9fb9c60eb57b01f7af6e3
Juan Chicaiza <juanpablochicaiza@firmasdigitales.com.co>
2022-01-08 14:22:35 -0500

    Test Services

--- src/test/java/com/example/Challenge_Op_Fuego/LocalizacionServiceTest.java
+++ src/test/java/com/example/Challenge_Op_Fuego/LocalizacionServiceTest.java
@@ -25,17 +25,6 @@ public class LocalizacionServiceTest {
     }
 
     @Test
-    public void getLocationSmall(){
-        double[][] positions = new double[][]{{1.0}, {2.0}, {3.0}};
-        double[] distances = new double[]{1.1, 0.1, 0.9};
-        double[] expectedPosition = new double[]{2.1};
-        double[] calculatedPosition = localizacionService.getLocation(positions, distances);
-        for (int i = 0; i < calculatedPosition.length; i++) {
-            assertEquals(expectedPosition[i], calculatedPosition[i]);
-        }
-    }
-
-    @Test
     public void getLocationWith4Positions(){
         double[][] positions = new double[][]{{5.0, -6.0}, {13.0, -15.0}, {21.0, -3.0}, {12.42, -21.2}};
         double[] distances = new double[]{8.06, 13.97, 23.32, 15.31};
